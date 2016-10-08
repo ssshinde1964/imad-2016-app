01a27d9401eba868ce0a8d9eebb12d72ef0faeef
ssshinde1964 <ssshinde@sggs.ac.in>
2016-10-08 18:22:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text'
     );
 element.innerHTML = 'New value';
+
+// move the image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 2;
+    img.style.marginLeft = marginLeft +'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+};
