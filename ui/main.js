a74ae8fc563a387c896603a872e5bda1f9431d3d
ssshinde1964 <ssshinde@sggs.ac.in>
2016-10-10 13:56:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,23 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function () {
-    // Make a request to the counter endpoint
+    // Create a request object
+    var request = newXMLHttpRequest();
     
     // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.ready.Sate === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('counter');
-    span.innerHTML = counter.toString();
+    // Make the request
+    request.open('GET', 'http://http://ssshinde1964.imad.hasura-app.io/counter', true);
+    request.send(null); 
 };
